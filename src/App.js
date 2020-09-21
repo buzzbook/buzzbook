f722393a9d31c97c8088348b612fc8858bb0bb9a
James Narayanan <jamesnarayanan@gmail.com>
2020-09-20 22:29:57 -0400

    Removed create react app default page
    Added temporary landing page
    Switched pages from spaces to tabs
    Added bootstrap
    Added favicon
    Moved css files and images to their own folders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/App.css";
+import buzz from "./img/buzzIcon.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="text-center">
+			<h1>
+				<b>
+					Georgia <span style={{color: "var(--gt-gold)"}}>Tech</span>
+				</b>{" "}
+				<i>BuzzBook</i>
+			</h1>
+			<h5>A reenvisioned course catalog, made by students, for students.</h5>
+			<img
+				src={buzz}
+				alt="buzz"
+				className="mx-auto"
+				style={{maxWidth: "40%"}}
+			/>
+		</div>
+	);
 }
 
 export default App;
