1609e5bdf352a7da106b0b9eb99a7cc04f2b6e09
jeffreytram <jeffrey_tram@yahoo.com>
2020-10-09 22:58:48 -0400

    Changed FilterItem to use composition in Catalog's Filter.js

--- src/components/FilterItem.js
+++ src/components/FilterItem.js
@@ -1,33 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import FilterSelector from "./FilterSelector";
 
-export default function FilterItem({ label, className, ariaLabel, ariaDescribedBy, defaultValue, onChange, optionList }) {
+export default function FilterItem({ children, label }) {
   return (
     <div className="input-group mb-3">
       <div className="input-group-prepend">
         <span className="input-group-text" id="sort-by-label">
           {label}
-				</span>
+        </span>
       </div>
-      <FilterSelector
-        className={className}
-        ariaLabel={ariaLabel}
-        ariaDescribedBy={ariaDescribedBy}
-        defaultValue={defaultValue}
-        onChange={onChange}
-        optionList={optionList}
-      />
+      {children}
     </div>
   );
 }
 
 FilterItem.propTypes = {
   label: PropTypes.string.isRequired,
-  className: PropTypes.string.isRequired,
-  ariaLabel: PropTypes.string.isRequired,
-  ariaDescribedBy: PropTypes.string.isRequired,
-  defaultValue: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  optionList: PropTypes.arrayOf(PropTypes.string).isRequired
 };
--- src/components/catalog/Filters.js
+++ src/components/catalog/Filters.js
@@ -34,15 +34,16 @@ function Filters() {
 				</span>
 			</div>
 			<input type="text" className="form-control mb-2" placeholder="Search" />
-      <FilterItem
-        label="Sort By"
-        className="custom-select"
-        ariaLabel="Sort By"
-        ariaDescribedBy="sort-by-label"
-        defaultValue="Course ID"
-        onChange={value => dispatch(updateSort(value))}
-        optionList={["Course ID", "Name", "Grade", "Difficulty", "Enrollment"]}
-      />
+      <FilterItem label="Sort By">
+        <FilterSelector
+          className="custom-select"
+          ariaLabel="Sort By"
+          ariaDescribedBy="sort-by-label"
+          defaultValue="Course ID"
+          onChange={value => dispatch(updateSort(value))}
+          optionList={["Course ID", "Name", "Grade", "Difficulty", "Enrollment"]}
+        />
+      </FilterItem>
 
 			<div id="catalog-filters">
 				<div className="mb-1 font-weight-bold">
@@ -55,72 +56,69 @@ function Filters() {
 						Clear
 					</span>
 				</div>
-        <FilterItem
-          label="Term"
-          className="custom-select"
-          ariaLabel="Term"
-          ariaDescribedBy="term-label"
-          defaultValue="Any"
-          onChange={value =>  updateFil(value, "term")}
-          optionList={["Any", "Fall 2020", "Spring 2021"]}
+        <FilterItem label="Term">
+          <FilterSelector 
+            className="custom-select"
+            ariaLabel="Term"
+            ariaDescribedBy="term-label"
+            defaultValue="Any"
+            onChange={value =>  updateFil(value, "term")}
+            optionList={["Any", "Fall 2020", "Spring 2021"]}
+          />
+        </FilterItem>
+        <FilterItem label="Credits">
+          <FilterSelector
+            className="custom-select"
+            ariaLabel="Credits"
+            ariaDescribedBy="credits-label"
+            defaultValue="Any"
+            onChange={value =>  updateFil(value, "credits")}
+            optionList={["Any", "1", "2", "3", "4"]}
+          />
+        </FilterItem>
+        <FilterItem label="Subject">
+          <FilterSelector
+            className="custom-select"
+            ariaLabel="Subject"
+            ariaDescribedBy="subject-label"
+            defaultValue="Any"
+            onChange={value =>  updateFil(value, "subject")}
+            optionList={["Any"].concat(
+              Object.keys(subjectNames).map(key => {
+                return subjectNames[key];
+            }))}
+          />
+        </FilterItem>
+        <FilterItem label="Level">
+          <FilterSelector
+            className="custom-select"
+            ariaLabel="Level"
+            ariaDescribedBy="level-label"
+            defaultValue="Any"
+            onChange={value =>  updateFil(value, "level")}
+            optionList={["Any", "1000", "2000", "3000", "4000"]}
+          />
+        </FilterItem>
+        <FilterItem label="Core">
+            <FilterSelector
+              className="custom-select"
+              ariaLabel="Core"
+              ariaDescribedBy="core-label"
+              defaultValue="Any"
+              onChange={value =>  updateFil(value, "core")}
+              optionList={["Any", "Test 1", "Test 2"]}
         />
-        <FilterItem
-          label="Credits"
-          className="custom-select"
-          ariaLabel="Credits"
-          ariaDescribedBy="credits-label"
-          defaultValue="Any"
-          onChange={value =>  updateFil(value, "credits")}
-          optionList={["Any", "1", "2", "3", "4"]}
-        />
-        <FilterItem
-          label="Subject"
-          className="custom-select"
-          ariaLabel="Subject"
-          ariaDescribedBy="subject-label"
-          defaultValue="Any"
-          onChange={value =>  updateFil(value, "subject")}
-          optionList={["Any"].concat(
-            Object.keys(subjectNames).map(key => {
-              return subjectNames[key];
-          }))}
-        />
-        <FilterItem
-          label="Level"
-          className="custom-select"
-          ariaLabel="Level"
-          ariaDescribedBy="level-label"
-          defaultValue="Any"
-          onChange={value =>  updateFil(value, "level")}
-          optionList={["Any", "1000", "2000", "3000", "4000"]}
-        />
-        <FilterItem
-          label="Core"
-          className="custom-select"
-          ariaLabel="Core"
-          ariaDescribedBy="core-label"
-          defaultValue="Any"
-          onChange={value =>  updateFil(value, "core")}
-          optionList={["Any", "Test 1", "Test 2"]}
-        />
-				<div className="input-group mb-3">
-					<div className="input-group-prepend">
-						<span className="input-group-text">Days</span>
-					</div>
-					<div className="button-group">
-						<button onClick={handleDayButtonClick}>M</button>
-						<button onClick={handleDayButtonClick}>T</button>
-						<button onClick={handleDayButtonClick}>W</button>
-						<button onClick={handleDayButtonClick}>R</button>
-						<button onClick={handleDayButtonClick}>F</button>
-					</div>
-				</div>
-				<div className="input-group mb-3">
-					<div className="input-group-prepend">
-						<span className="input-group-text" id="time-label">
-							Time
-						</span>
-					</div>
+        </FilterItem>
+        <FilterItem label="Days">
+          <div className="button-group">
+            <button onClick={handleDayButtonClick}>M</button>
+            <button onClick={handleDayButtonClick}>T</button>
+            <button onClick={handleDayButtonClick}>W</button>
+            <button onClick={handleDayButtonClick}>R</button>
+            <button onClick={handleDayButtonClick}>F</button>
+          </div>
+        </FilterItem>
+				<FilterItem label="Time">
           <FilterSelector
             className="custom-select"
             ariaLabel="Time-from"
@@ -137,16 +135,18 @@ function Filters() {
             onChange={value =>  updateFil(value, "time-to")}
             optionList={["Any", "11:00", "12:00"]}
           />
-				</div>
-        <FilterItem
-          label="Prof"
-          className="custom-select"
-          ariaLabel="Prof"
-          ariaDescribedBy="prof-label"
-          defaultValue="Any"
-          onChange={value =>  updateFil(value, "prof")}
-          optionList={["Any", "Mr. Smith", "Mr.Brown"]}
-        />
+        </FilterItem>
+					
+        <FilterItem label="Prof">
+          <FilterSelector
+            className="custom-select"
+            ariaLabel="Prof"
+            ariaDescribedBy="prof-label"
+            defaultValue="Any"
+            onChange={value =>  updateFil(value, "prof")}
+            optionList={["Any", "Mr. Smith", "Mr.Brown"]}
+          />
+        </FilterItem>
 			</div>
 		</div>
 	);
