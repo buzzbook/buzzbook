e6ff0e1902f31bb36494ef22ee13f3a9394ef2b5
jeffreytram <jeffrey_tram@yahoo.com>
2020-10-15 15:09:56 -0400

    Changed the Grades and Catalog page to use the Layout component

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,30 +1,19 @@
-import React, {useLayoutEffect} from "react";
+import React from "react";
 import Filters from "../components/catalog/Filters";
 import CourseList from "../components/catalog/CourseList";
 import CourseInfo from "../components/catalog/CourseInfo";
+import Layout from "./Layout";
 
 import "../css/Catalog.css";
 
 function Catalog() {
-	useLayoutEffect(() => {
-		let navHeight =
-			document.getElementById("navbar").getBoundingClientRect().height + 2;
-		document.getElementById(
-			"catalog"
-		).style.height = `calc(100vh - ${navHeight}px)`;
-	});
 	return (
-		<div className="col-page row mx-0" id="catalog">
-			<div className="col-3 h-100 p-3">
-				<Filters />
-			</div>
-			<div className="col-3 h-100 p-3">
-				<CourseList />
-			</div>
-			<div className="col-6 h-100 p-3">
-				<CourseInfo />
-			</div>
-		</div>
+    <Layout 
+      id="Catalog"
+      col1Content={<Filters/>}
+      col2Content={<CourseList />}
+      col3Content={<CourseInfo />}
+    />
 	);
 }
 
--- src/pages/Grades.js
+++ src/pages/Grades.js
@@ -1,31 +1,25 @@
-import React, {useLayoutEffect} from "react";
+import React from "react";
 import Filters from "../components/grades/Filters";
 import SelectedCourses from "../components/grades/SelectedCourses";
 import "../css/Grades.css";
 import CourseList from "../components/grades/CourseList";
+import Layout from "./Layout";
 
 function Grades() {
-	useLayoutEffect(() => {
-		let navHeight =
-			document.getElementById("navbar").getBoundingClientRect().height + 2;
-		document.getElementById(
-			"grades"
-		).style.height = `calc(100vh - ${navHeight}px)`;
-	});
-	return (
-		<div className="col-page row mx-0" id="grades">
-			<div className="col-3 h-100 p-3">
-				<Filters />
-				<CourseList />
-			</div>
-			<div className="col-3 h-100 p-3">
-				<SelectedCourses />
-			</div>
-			<div className="col-6 h-100 p-3">
-				<h1>Graph</h1>
-			</div>
-		</div>
-	);
+
+  return (
+    <Layout
+      id="grades"
+      col1Content={(
+        <div>
+          <Filters />
+          <CourseList />
+        </div>
+      )}
+      col2Content={<SelectedCourses />}
+      col3Content={<h1>Graph</h1>}
+    />
+  );
 }
 
 export default Grades;
