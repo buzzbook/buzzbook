bd742772c48ea21c220e65c379ff5993f9ee6527
jeffreytram <jeffrey_tram@yahoo.com>
2020-11-28 06:57:26 -0500

    Added visible state to DropdownButton

--- src/components/filters/DropdownButton.js
+++ src/components/filters/DropdownButton.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import Tippy from '@tippyjs/react';
 import '../../css/DropdownButton.css';
 
 export default function DropdownButton({ children, label, className }) {
+  const [visible, setVisible] = useState(false);
+  const show = () => setVisible(true);
+  const hide = () => setVisible(false);
   return (
-    <Tippy content={<div className="dropdownContainer">{children}</div>}  trigger='click' interactive placement='bottom-start' delay={[0,0]}>
-      <button className={className}>{label}</button>
+    <Tippy content={<div className='dropdownContainer'>{children}</div>}
+      trigger='click'
+      interactive
+      placement='bottom-start'
+      onShow={show}
+      onHide={hide}
+    >
+      <button className={`${className} ${visible ? 'shownFilter' : ''}`}>{label}</button>
     </Tippy>
   );
 }
