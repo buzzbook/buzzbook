4f5de1109946af604d793dc7fa696153b73e9bb9
jeffreytram <jeffrey_tram@yahoo.com>
2020-12-11 07:27:18 -0500

    Updated ExpandedFilters to be consistent with Filters

--- src/components/filters/ExpandedFilters.js
+++ src/components/filters/ExpandedFilters.js
@@ -9,10 +9,13 @@ import {
 } from "../../redux/courseListSlice";
 import {subjectNames} from "../courseList/CourseList";
 import FilterSelector from "./FilterSelector";
+import {instructors, caches} from "../../courses";
+
 
 function ExpandedFilters() {
 	const clearFilters = () => {
-		dispatch(resetFilters());
+    dispatch(resetFilters());
+    dispatch(updateSort({label: "Course ID", value: "Course ID"}))
 	};
 
 	const dispatch = useDispatch();
@@ -48,36 +51,59 @@ function ExpandedFilters() {
 				/>
 				<h4 className="mt-2">Department</h4>
 				<FilterSelector
-					ariaLabel="Subject"
-					ariaDescribedBy="subject-label"
-					placeholder="Select..."
-					value={filters.subject}
-					onChange={value => updateFil(value, "subject")}
-					optionList={Object.keys(subjectNames).map(key => {
-						return subjectNames[key];
-					})}
-					isMulti={true}
-				/>
+          ariaLabel="Subject"
+          ariaDescribedBy="subject-label"
+          placeholder="Search..."
+          value={filters.subject}
+          onChange={value => updateFil(value, "subject")}
+          optionList={Object.keys(subjectNames).map(key => {
+            return subjectNames[key];
+          })}
+          isMulti
+          isSearchable
+          windowed
+        />
 				<h4 className="mt-2">Level</h4>
 				<FilterSelector
-					ariaLabel="Level"
-					ariaDescribedBy="level-label"
-					placeholder="Select..."
-					value={filters.level}
-					onChange={value => updateFil(value, "level")}
-					optionList={[1000, 2000, 3000, 4000, 6000, 7000, 8000, 9000]}
-					isMulti={true}
-				/>
+          ariaLabel="Level"
+          ariaDescribedBy="level-label"
+          value={filters.level}
+          onChange={value => updateFil(value, "level")}
+          optionList={[1000, 2000, 3000, 4000, 6000, 7000, 8000, 9000]}
+          isMulti
+        />
 				<h4 className="mt-2">Credits</h4>
 				<FilterSelector
-					ariaLabel="Credits"
-					ariaDescribedBy="credits-label"
-					placeholder="Select..."
-					value={filters.credits}
-					onChange={value => updateFil(value, "credits")}
-					optionList={[0, 1, 2, 3, 4, 5, 6, 9]}
-					isMulti={true}
-				/>
+          ariaLabel="Credits"
+          ariaDescribedBy="credits-label"
+          value={filters.credits}
+          onChange={value => updateFil(value, "credits")}
+          optionList={[0, 1, 2, 3, 4, 5, 6, 9]}
+          isMulti
+        />
+        <h4 className="mt-2">Type</h4>
+        <FilterSelector
+          ariaLabel="Type"
+          ariaDescribedBy="type-label"
+          placeholder="Search..."
+          value={filters.type}
+          onChange={value => updateFil(value, "type")}
+          optionList={caches["scheduleTypes"]}
+          isMulti
+          isSearchable
+        />
+        <h4 className="mt-2">Instructors</h4>
+        <FilterSelector
+          ariaLabel="Instructors"
+          ariaDescribedBy="instructors-label"
+          placeholder="Search..."
+          value={filters.instructors}
+          onChange={value => updateFil(value, "instructors")}
+          optionList={instructors}
+          isMulti
+          isSearchable
+          windowed
+        />
 			</div>
 		</div>
 	);
