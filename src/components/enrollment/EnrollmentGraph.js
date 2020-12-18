3853c2872cb3ed1f5682fe94f445c1a7c260c6c3
jeffreytram <jeffrey_tram@yahoo.com>
2020-12-18 15:01:48 -0500

    Chart js setup with basic example

--- src/components/enrollment/EnrollmentGraph.js
+++ src/components/enrollment/EnrollmentGraph.js
@@ -1,28 +1,50 @@
-import React from "react";
-import {useSelector} from "react-redux";
-import {getSavedCourses} from "../../redux/courseListSlice";
+import React, { useEffect } from "react";
+import Chart from 'chart.js';
 // Webpack import not working for some reason
 // import {graphic} from "../../img/gradesPageGraphic.png";
 
 function EnrollmentGraph() {
-	const savedCourses = useSelector(getSavedCourses);
 
-	if (Object.keys(savedCourses).length === 0) {
-		return (
-			<div className="text-center">
-				<br />
-				<h3>Save some courses to get started!</h3>
-				<br />
-				<img
-					src={require("../../img/gradesPageGraphic.png")}
-					alt="select courses"
-					style={{maxHeight: "60vh", width: "auto"}}
-				/>
-			</div>
-		);
-	} else {
-		return <h1>Enrollment Graph</h1>;
-	}
+  useEffect(() => {
+    const ctx = document.getElementById('enrollment-graph');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }, []);
+  return <canvas id="enrollment-graph" width="400" height="400"></canvas>;
 }
 
 export default EnrollmentGraph;
