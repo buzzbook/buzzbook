11f73cc53d247d2d1bad9c6e21ce2933aef7d29d
jeffreytram <jeffrey_tram@yahoo.com>
2021-01-02 08:31:34 -0500

    Added day, enrollment, and waitlist data to Enrollment graph tooltip

--- src/components/enrollment/EnrollmentGraph.js
+++ src/components/enrollment/EnrollmentGraph.js
@@ -6,37 +6,37 @@ import Chart from 'chart.js';
 // x is days since phase 1
 // y is enrollment percent
 const data1 = [
-  { x: 0, y: 0 },
-  { x: 1, y: 11 },
-  { x: 2, y: 30 },
-  { x: 3, y: 65 },
-  { x: 4, y: 95 },
-  { x: 5, y: 100 },
-  { x: 6, y: 100 },
-  { x: 7, y: 99 },
-  { x: 8, y: 100 },
-  { x: 9, y: 100 },
-  { x: 10, y: 100 },
-  { x: 11, y: 98 },
-  { x: 12, y: 100 },
-  { x: 13, y: 100 },
+  { x: 0, y: 0, waitlist: 0 },
+  { x: 1, y: 11, waitlist: 0 },
+  { x: 2, y: 30, waitlist: 0 },
+  { x: 3, y: 65, waitlist: 0 },
+  { x: 4, y: 95, waitlist: 0 },
+  { x: 5, y: 100, waitlist: 1 },
+  { x: 6, y: 100, waitlist: 5 },
+  { x: 7, y: 99, waitlist: 10 },
+  { x: 8, y: 100, waitlist: 9 },
+  { x: 9, y: 100, waitlist: 7 },
+  { x: 10, y: 100, waitlist: 8 },
+  { x: 11, y: 98, waitlist: 9 },
+  { x: 12, y: 100, waitlist: 8 },
+  { x: 13, y: 100, waitlist: 8 },
 ];
 
 const data2 = [
-  { x: 0, y: 0 },
-  { x: 1, y: 2 },
-  { x: 2, y: 10 },
-  { x: 3, y: 15 },
-  { x: 4, y: 29 },
-  { x: 5, y: 40 },
-  { x: 6, y: 49 },
-  { x: 7, y: 59 },
-  { x: 8, y: 63 },
-  { x: 9, y: 67 },
-  { x: 10, y: 69 },
-  { x: 11, y: 72 },
-  { x: 12, y: 73 },
-  { x: 13, y: 74 },
+  { x: 0, y: 0, waitlist: 0 },
+  { x: 1, y: 2, waitlist: 0 },
+  { x: 2, y: 10, waitlist: 0 },
+  { x: 3, y: 15, waitlist: 0 },
+  { x: 4, y: 29, waitlist: 0 },
+  { x: 5, y: 40, waitlist: 0 },
+  { x: 6, y: 49, waitlist: 0 },
+  { x: 7, y: 59, waitlist: 0 },
+  { x: 8, y: 63, waitlist: 0 },
+  { x: 9, y: 67, waitlist: 0 },
+  { x: 10, y: 69, waitlist: 0 },
+  { x: 11, y: 72, waitlist: 0 },
+  { x: 12, y: 73, waitlist: 0 },
+  { x: 13, y: 74, waitlist: 0 },
 ];
 
 function EnrollmentGraph() {
@@ -47,14 +47,14 @@ function EnrollmentGraph() {
       type: 'line',
       data: {
         datasets: [{
-          label: 'Class 1',
+          label: 'ACCT 2102',
           data: data1,
           borderColor: 'rgba(255, 99, 132, 1)',
           borderWidth: 1,
           fill: false,
         },
         {
-          label: 'Class 2',
+          label: 'CS 1100',
           data: data2,
           borderColor: 'rgba(54, 162, 235, 1)',
           borderWidth: 1,
@@ -91,8 +91,33 @@ function EnrollmentGraph() {
               callback: (label) => label + '%',
             }
           }]
-        }
-      }
+        },
+        tooltips: {
+          callbacks: {
+            title: function () {
+              return ``;
+            },
+            label: function (tooltipItem, data) {
+              const line = data.datasets[tooltipItem.datasetIndex];
+              const className = line.label;
+              return `${className}`;
+            },
+            afterBody: function (tooltipPoints, data) {
+              const tooltipItem = tooltipPoints[0];
+              const rawLine = data.datasets[tooltipItem.datasetIndex];
+              
+              // index of the item corresponds with the x value of the data
+              // i.e. Day 15 = the 15th index in data array
+              const rawItem = rawLine.data[parseInt(tooltipItem.label)];
+              const day = rawItem.x;
+              const enrollment = rawItem.y;
+              const waitlist = rawItem.waitlist;
+              let testMultiline = [`Day ${day}`, `${enrollment}% enrolled`, `${waitlist} waitlist`];
+              return testMultiline;
+            },
+          }
+        },
+      },
     });
   }, []);
   return <canvas id="enrollment-graph" width="400" height="400"></canvas>;
